fe517ba0ada12c1955a4d9f9e79975212b1e1fa3
Rakesh Karagwal <rakeshkaragwal@rathi.com>
2023-05-22 17:59:12 +0530

    file add

---
+++ ReportView.xaml.cs
@@ -0,0 +1,24 @@
+namespace Grievance;
+
+public partial class ReportView : ContentPage
+{
+	public ReportView()
+	{
+		InitializeComponent();
+	}
+
+    private void addNoteButton_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void addNoteSubmit_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void complaintDetailBack_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
---
+++ SearchView.xaml.cs
@@ -0,0 +1,19 @@
+namespace Grievance;
+
+public partial class SearchView : ContentPage
+{
+	public SearchView()
+	{
+		InitializeComponent();
+	}
+
+    private void dashboardSearchButton_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void dashboardSearchButton_Clicked_1(object sender, EventArgs e)
+    {
+
+    }
+}
